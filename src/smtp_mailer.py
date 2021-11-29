d26c1e4cba85ce56691ab4ced6eac8c3df400bff
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2021-11-29 18:57:10 +0530

    update smtp logging and other bug fixes

--- src/smtp_mailer.py
+++ src/smtp_mailer.py
@@ -24,22 +24,26 @@ SMTP_SERVER = os.environ.get('SMTP_SERVER', None)
 # SSM Parameter name which holds SMTP server password
 SMTP_PASSWORD_PARAMETER = os.environ.get('SMTP_PASSWORD_PARAMETER')
 
-def send_via_ssl(mailFrom, smtpPassword, mailTo, mailBody):
+def send_via_ssl(userName, mailFrom, smtpPassword, mailTo, mailBody):
     try:
+        logger.info('Sending mail to {} ({}) via SMTP over SSL'.format(userName, mailTo))
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT, context=context) as server:
             server.login(mailFrom, smtpPassword)
             server.sendmail(mailFrom, mailTo, mailBody)
+        logger.info('Mail sent to {} ({}) via SMTP over SSL'.format(userName, mailTo))
     except Exception as e:
         logger.error('Unable to send mail via SSL to {}. Reason: {}'.format(mailTo, e))
 
-def send_via_tls(mailFrom, smtpPassword, mailTo, mailBody):
+def send_via_tls(userName, mailFrom, smtpPassword, mailTo, mailBody):
     try:
+        logger.info('Sending mail to {} ({}) via SMTP over TLS'.format(userName, mailTo))
         context = ssl.create_default_context()
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls(context=context)
             server.login(mailFrom, smtpPassword)
             server.sendmail(mailFrom, mailTo, mailBody)
+        logger.info('Mail sent to {} ({}) via SMTP over TLS'.format(userName, mailTo))
     except Exception as e:
         logger.error('Unable to send mail via TLS to {}. Reason: {}'.format(mailTo, e))
 
@@ -67,10 +71,8 @@ def send_email(mailTo, userName, mailSubject, mailFrom, mailBodyPlain, mailBodyH
     message.attach(mailBodyHtml)
 
     if SMTP_PROTOCOL.lower() == 'ssl':
-        logger.info('Using SSL for sending mail to {}'.format(userName))
-        send_via_ssl(mailFrom, smtpPassword, mailTo, message.as_string())
+        send_via_ssl(userName, mailFrom, smtpPassword, mailTo, message.as_string())
     elif SMTP_PROTOCOL.lower() == 'tls':
-        logger.info('Using TLS for sending mail to {}'.format(userName))
-        send_via_ssl(mailFrom, smtpPassword, mailTo, message.as_string())
+        send_via_tls(userName, mailFrom, smtpPassword, mailTo, message.as_string())
     else:
         logger.error('{} is not a supported SMTP protocol'.format(SMTP_PROTOCOL))
