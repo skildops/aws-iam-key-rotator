a2a11a99988d47c13c39b2056d682674b518e4e7
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2022-04-21 12:25:13 +0530

    add shared function file

---
+++ src/shared_functions.py
@@ -0,0 +1,14 @@
+import boto3
+import os
+import logging
+
+logger = logging.getLogger('shared_functions')
+logger.setLevel(logging.INFO)
+
+iam = boto3.client('iam', region_name=os.environ.get('AWS_REGION'))
+
+def fetch_account_info():
+    return {
+        'id': os.environ['AWS_ACCOUNT_ID'],
+        'name': iam.list_account_aliases()['AccountAliases'][0]
+    }
