34d393ba80c55286a180d327fb6ff4b8c0084059
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2024-03-10 20:45:58 +0000

    lint shared functions file

--- src/shared_functions.py
+++ src/shared_functions.py
@@ -1,14 +1,24 @@
-import boto3
+"""
+Functions that are required by both creator and destructor modules
+"""
+
 import os
 import logging
+import boto3
 
-logger = logging.getLogger('shared_functions')
+logger = logging.getLogger("shared_functions")
 logger.setLevel(logging.INFO)
 
-iam = boto3.client('iam', region_name=os.environ.get('AWS_REGION'))
+iam = boto3.client("iam", region_name=os.environ.get("AWS_REGION"))
+
 
 def fetch_account_info():
+    """
+    Retrieve AWS account info
+    """
     return {
-        'id': os.environ.get('ACCOUNT_ID', ''),
-        'name': iam.list_account_aliases()['AccountAliases'][0] if len(iam.list_account_aliases()['AccountAliases']) > 0 else ''
+        "id": os.environ.get("ACCOUNT_ID", ""),
+        "name": iam.list_account_aliases()["AccountAliases"][0]
+        if len(iam.list_account_aliases()["AccountAliases"]) > 0
+        else "",
     }
